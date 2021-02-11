42f970e04a43b6ffaf923167425c01600158dcf0
Ademrobert <adembenothman@gmail.com>
2021-02-11 12:41:57 +0200

    Small chnages

--- src/utilities.js
+++ src/utilities.js
@@ -9,7 +9,7 @@ export const drawRect = (detections, ctx) =>{
     // Set styling
     const color = Math.floor(Math.random()*16777215).toString(16);
     ctx.strokeStyle = '#' + color
-    ctx.font = '18px Arial';
+    ctx.font = '20px Arial';
 
     // Draw rectangles and text
     ctx.beginPath();
